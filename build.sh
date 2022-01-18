b959e99846cd838a5ffd45f79e8558bbfbc0f5d2
venkatveginati <95076345+venkatveginati@users.noreply.github.com>
2022-01-18 11:24:25 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ VERSION=$(date +%H-%M-%S)
 docker build -t sreeharshav/testingbuild:${VERSION} .
 docker push sreeharshav/testingbuild:${VERSION}
 docker -H tcp://10.1.1.200:2375 stop nginx
-docker -H tcp://10.1.1.200:2375 run --rm -dit -p 8000:80 --name nginx --hostname nginx sreeharshav/testingbuild:${VERSION}
+docker -H tcp://10.1.1.200:2375 run --rm -dit -p 8000:80 --name nginx --hostname Nginx sreeharshav/testingbuild:${VERSION}
 echo "---------------------"
